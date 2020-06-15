7add309d907c03840282a5700d522f02ff83ae32
Lirent <pone@lirent.net>
2020-06-15 20:39:46 +0200

    NEW: Initial commit

---
+++ src/test/java/net/lirent/sbt/SpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.lirent.sbt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
